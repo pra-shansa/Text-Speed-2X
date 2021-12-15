d738f4dd036649795e0c3d466dceac8011565ee3
pra-shansa <77281208+pra-shansa@users.noreply.github.com>
2021-12-15 15:22:32 +0530

    url, text , youtube captions options added

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-import htf
-# htf.hugging_face()
+import yttranscripts
 import hft2
-t="https://en.wikipedia.org/wiki/Bharatanatyam"
-# t= "https://hackernoon.com/an-introduction-to-the-internet-computer"
-# t="""Bharatanatyam is a major form of Indian classical dance that originated in the modern-day region of Tamil Nadu. The Natya Shastra by Bharata Muni and Abhinaya Darpana (Mirror of Gesture) by Nandikeshvara are considered to be the original sources of Bharatanatyam (an Indian classical dance form). The dance form is also briefly mentioned in Kannada text Manasolalla written by Someshwara III. It has flourished in the temples and courts of southern India since ancient times.[1][2][3] It is one of eight widely recognized Indian classical dance forms (the others being Kathak, Kuchipudi, Odissi, Kathakali, Mohiniyattam, Manipuri and Sattriya) and it expresses South Indian religious themes and spiritual ideas, particularly of Shaivism, Vaishnavism and Shaktism.[1][4][5]
-# A description of Bharatanatyam from the 2nd century CE can be found in the ancient Tamil epic Silappatikaram, while temple sculptures of the 6th to 9th century CE suggest it was a highly refined performance art by the mid-1st millennium CE.[6][7] Bharatanatyam is the oldest classical dance tradition in India.[8] The dance form was prevalent in ancient Tamil Nadu, and several books have codified it, such as the Natya Shastra[4] by Bharat Muni (Sanskrit: भरत मुनि). Bharatanatyam is the state dance form of Tamil Nadu.
+t = "https://en.wikipedia.org/wiki/Bharatanatyam"
+opt = 2
+v= "sAzL4XMke80"
 
-# Bharatanatyam contains different types of banis. Bani, or 'tradition', is a term used to describe the dance technique and style specific to a guru or school. These are named according to the village of the guru (with the exception of some banis). Bharatanatyam style is noted for its fixed upper torso, bent legs and knees flexed (Aramandi) combined with spectacular footwork, and a sophisticated vocabulary of sign language based on gestures of hands, eyes, and face muscles.[7] The dance is accompanied by music and a singer, and typically the dancer's guru is present as the Nattuvanar, director, and conductor of the performance and art.[1] The dance has traditionally been a form of an interpretive narration of mythical legends and spiritual ideas from Hindu texts.[9] The performance repertoire of Bharatanatyam, like other classical dances, includes nrita (pure dance), nritya (solo expressive dance) and natya (group dramatic dance).[9][10]
-
-# Bharatanatyam remained exclusive to Hindu temples through the 19th century.[7] It was banned by the colonial British government in 1910,[11] but the Indian community protested against the ban and expanded its performance outside temples in the 20th century.[7][11][12] Modern stage productions of Bharatanatyam has been spread out and popular throughout India that has been done in different ways and have incorporated technical performances, pure dance based on non-religious ideas and fusion themes.[6][7] Bharatanatyam was also known as the dance of the devadasi clan to entice the royals in olden days."""
-print(hft2.hugging_face(t,1))
-#summary
-#text url
-#youtube
-#headline
-#scientific
-#proof finding, direct lines
-#recipe
+if opt <= 1:
+    print(hft2.hugging_face(t,opt))
+elif opt == 2:
+    print(hft2.hugging_face(yttranscripts.youtube_cap(v),0))
